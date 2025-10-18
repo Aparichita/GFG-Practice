36f1f69adf5d663a2707ac5e1a77fb327614ad98
Aparichita <143521076+Aparichita@users.noreply.github.com>
2025-10-19 00:47:19 +0530

    ReversefirstKofaQueue.java

---
+++ ReversefirstKofaQueue.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public Queue<Integer> reverseFirstK(Queue<Integer> q, int k) {
+        // code here
+        Stack<Integer>st=new Stack<>();
+        if(q.isEmpty() || k<=0 || k>q.size()){
+            return q;
+        }
+        
+        
+        int n=q.size();
+       
+        for(int i=0;i<k;i++){
+            st.push(q.remove());
+        }
+        while(!st.isEmpty()){
+            q.add(st.pop());
+            
+        }
+        for(int i=0;i<n-k;i++){
+            q.add(q.remove());
+        }
+        return q;
+    }
+}
